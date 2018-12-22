e7cce67344df1a8d4df85f6cb7546cc7156d5255
Tim Becker <timbckr@posteo.net>
2018-12-22 17:50:56 +0100

    change defaults

--- templates/go-project/lazybones.groovy
+++ templates/go-project/lazybones.groovy
@@ -2,9 +2,9 @@ def props = [:]
 
 def date = new Date()
 props.currentyear = date[Calendar.YEAR]
-props.name = ask("What is your name? [Tim Becker]: ", "Tim Becker", "name")
-props.nickname = ask("What is your Github/Gitlab nickname? [zorck97]: ", "zorck97", "nickname")
-props.email = ask("What is your email? [timbckr@posteo.net]: ", "timbckr@posteo.net", "email")
+props.name = ask("What is your name? [Max Mustermann]: ", "Max Mustermann", "name")
+props.nickname = ask("What is your Github/Gitlab nickname? [your-nickname]: ", "your-nickname", "nickname")
+props.email = ask("What is your email? [your-email]: ", "your-email", "email")
 props.projectname = ask("Define value for 'projectname' [projectname]: ", "projectname", "projectname")
 props.version = ask("Define value for 'version' [v0.0.0]: ", "v0.0.0", "version")
 
